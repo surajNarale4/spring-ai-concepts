b72e81cf3ca8fc17660de834bd74ba9629789778
surajNarale4 <rajnarale415@gmail.com>
2026-09-19 13:06:04 +0530

    feat: small rag implementation added for understanding

--- src/main/java/com/prod/services/AiService.java
+++ src/main/java/com/prod/services/AiService.java
@@ -7,10 +7,13 @@ import com.prod.dto.AiRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.template.st.StTemplateRenderer;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
@@ -20,6 +23,7 @@ import reactor.core.publisher.Flux;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -83,12 +87,54 @@ public class AiService {
                         .query(text)
                         .topK(2)
                         .similarityThreshold(0.7)
+                        /*
+                        we can add expression
+                        if we vector data for spring ai
+                        and some movies besed metadata we can make filter out
+                        our vectors get documents
+                         */
 //                        .filterExpression(b.and(
 //                                b.in("author","john", "jill"),
 //                                b.eq("article_type", "blog")).build())
                         .build()
 
         );
+    }
+
+    public String askAiRAG(String text){
+
+        String template = """
+    You are a movie recommendation system.
+    and your name is :'suraj ai system'
+    Use the following movie information to answer the user's question.
+
+    Context:
+    '{context}'
+
+    Recommend a movie should based on the context only.
+   
+""";
+
+
+        List<Document> documents= similaritySearch(text);
+        String context = documents.stream()
+                .map(doc->doc.getText())
+                .collect(Collectors.joining("\n"));
+
+//        promptTemplate.add("question",text);
+//        promptTemplate.add("context",context);
+
+
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        String systemPrompt = promptTemplate.render(Map.of("context",context));
+        System.out.println("System Promp :"+systemPrompt);
+
+        return openAiChatClient
+                .prompt()
+                .system(systemPrompt)
+                .user(text)
+                .call()
+                .content();
     }
 
     public String getJoke(String topic){
